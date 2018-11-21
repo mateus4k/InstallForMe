f5091c9042e1437f119812de8be401cfc94d4cd6
mateus4k <mateus4k@protonmail.ch>
2018-11-21 10:42:05 -0300

    :rocket: Update

--- installforme.sh
+++ installforme.sh
@@ -169,7 +169,7 @@ zsh(){
 
 nodejs(){
 	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get install -y nodejs
+    sudo apt-get install -y nodejs npm
     sudo npm install npm@latest -g
 	menu
 }
