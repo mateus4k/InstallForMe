9a4ff58178e74c527aa0a6145833e931026b6722
mateus4k <mateus4k@protonmail.ch>
2018-11-21 09:20:28 -0300

    :rocket: Update NodeJS to LTS

--- installforme.sh
+++ installforme.sh
@@ -168,9 +168,10 @@ zsh(){
 }
 
 nodejs(){
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-    sudo apt-get install -y nodejs npm
-    menu
+	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+    sudo npm install npm@latest -g
+	menu
 }
 
 chrome(){
