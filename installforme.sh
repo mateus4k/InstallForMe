fb184d21aea2de06a5054b5773376dc738890da9
Mateus4k <mateus4k@protonmail.ch>
2019-07-03 17:28:48 -0300

    Add filezilla in others instruction

--- installforme.sh
+++ installforme.sh
@@ -335,7 +335,7 @@ php(){
 }
 
 others(){
-    sudo apt-get -y install gedit neofetch unrar ttf-mscorefonts-installer transmission synapse net-tools
+    sudo apt-get -y install gedit neofetch unrar ttf-mscorefonts-installer transmission synapse net-tools filezilla
     sudo fc-cache
     menu
 }
