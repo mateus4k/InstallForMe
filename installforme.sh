cb53f927a972de6b3dfe2e30048a18496895012b
mateus4k <mateus4k@protonmail.ch>
2019-07-23 12:26:00 -0300

    :bug: Fixed NodeJs installer and add Yarn installer

--- installforme.sh
+++ installforme.sh
@@ -155,32 +155,32 @@ vim(){ #by SergioPrado.org
 
 
 zsh(){
-    sudo apt install -y zsh
+	sudo apt install -y zsh
 
-    # delete old files
-    sudo rm -rf ~/.oh-my-zsh
+	# delete old files
+	sudo rm -rf ~/.oh-my-zsh
 
-    # download oh-my-zsh
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+	# download oh-my-zsh
+	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-    # download autosuggestions plugin
+	# download autosuggestions plugin
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-    # download syntax-highlighting plugin 
+	# download syntax-highlighting plugin 
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-    # download dracula theme 
+	# download dracula theme 
 	git clone https://github.com/dracula/zsh.git dracula
 	sudo mv dracula/dracula.zsh-theme ~/.oh-my-zsh/themes/
 	mkdir ~/.oh-my-zsh/themes/lib/
 	sudo mv dracula/lib/* ~/.oh-my-zsh/themes/lib/     
 	rm -rf dracula
 
-    # install agnoster theme font
-    sudo apt install -y fonts-powerline
+	# install agnoster theme font
+	sudo apt install -y fonts-powerline
 
-    # install zsh config file
-    sudo cp zshrc ~/.zshrc
+	# install zsh config file
+	sudo cp zshrc ~/.zshrc
 
 	# for root
 	sudo usermod -s $(which zsh) root
@@ -188,13 +188,13 @@ zsh(){
 	# for current user
 	sudo usermod -s $(which zsh) ${USER}
 
-    menu
+	menu
 }
 
 nodejs(){
-	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get install -y nodejs npm
-    sudo npm install npm@latest -g
+	sudo apt-get install -y nodejs npm
+	sudo npm install npm@latest -g
+	sudo apt-get install -y yarn
 	menu
 }
 
