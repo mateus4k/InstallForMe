c8e85332c02f6da4965afa86ebb5483ae070a711
mateus4k <mateus4k@protonmail.ch>
2019-09-02 11:10:28 -0300

    :package: Add dracula colorscheme for Gnome Terminal

--- src/ohmyzsh.sh
+++ src/ohmyzsh.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-sudo apt install -y zsh
-
 # delete old files
 sudo rm -rf ~/.oh-my-zsh
 
-# download oh-my-zsh
+# install packages
+sudo apt install -y zsh dconf-cli fonts-powerline
+
+# oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-# download autosuggestions plugin
+# autosuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-# download syntax-highlighting plugin
+# syntax-highlighting plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-# download dracula theme
+# dracula theme
 git clone https://github.com/dracula/zsh.git dracula
 sudo mv dracula/dracula.zsh-theme ~/.oh-my-zsh/themes/
 mkdir ~/.oh-my-zsh/themes/lib/
 sudo mv dracula/lib/* ~/.oh-my-zsh/themes/lib/
 rm -rf dracula
-
-# install agnoster theme font
-sudo apt install -y fonts-powerline
 
 # install zsh config file
 sudo cp zshrc ~/.zshrc
@@ -32,5 +30,10 @@ sudo usermod -s $(which zsh) root
 
 # for current user
 sudo usermod -s $(which zsh) ${USER}
+
+# dracula colorscheme for Gnome Terminal
+git clone https://github.com/GalaticStryder/gnome-terminal-colors-dracula
+cd gnome-terminal-colors-dracula
+./install.sh
 
 menu
